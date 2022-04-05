06de60ca4cdda01fb6fd563d4be405b16caa490b
Brooks Ryba <brooksryba@gmail.com>
2022-04-05 02:42:35 -0400

    Remove unused code

--- Assets/Classes/DynamicMenu/DynamicMenuItem.cs
+++ Assets/Classes/DynamicMenu/DynamicMenuItem.cs
@@ -9,17 +9,6 @@ using TMPro;
 public class DynamicMenuItem : MonoBehaviour
 {
     public GameObject menuText;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void SetLabel(string label)
     {
