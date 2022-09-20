a9a820413885f073f0afef81577e5260436971aa
Brooks Ryba <brooksryba@gmail.com>
2022-09-19 22:42:34 -0400

    Fix player portal position bug

--- Assets/Classes/Controllers/Player/CharacterMovement.cs
+++ Assets/Classes/Controllers/Player/CharacterMovement.cs
@@ -39,6 +39,7 @@ public class CharacterMovement : MonoBehaviour
                 LoadState();
                 if(playerScriptable.ready) {
                     transform.position = playerScriptable.Read();
+                    position = transform.position;
                 } else {
                     if(!(position.x == 0 && position.y == 0)) {
                         transform.position = position;
