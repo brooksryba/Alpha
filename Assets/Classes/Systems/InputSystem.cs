5144fb3fa3f49b6827134bbb10799fb451d136e3
Brooks Ryba <brooksryba@gmail.com>
2022-10-02 17:12:57 -0400

    Better mobile controls

--- Assets/Classes/Systems/InputSystem.cs
+++ Assets/Classes/Systems/InputSystem.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class InputSystem : MonoBehaviour
 {
+    public static InputSystem instance { get; private set; }
+    public void OnEnable() { instance = this; }
+
+    private bool useOverlay = true;
     public GameObject overlayObject;
+
     void Start()
     {
         if(SystemInfo.deviceType != DeviceType.Handheld){
+            useOverlay = false;
             overlayObject.SetActive(false);
         }        
+    }
+
+    public void DisableControls()
+    {
+        if(useOverlay && overlayObject != null) {
+            overlayObject.SetActive(false);
+        }
+    }
+
+    public void EnableControls()
+    {
+        if(useOverlay && overlayObject != null) {
+            overlayObject.SetActive(true);
+        }
     }
 }
