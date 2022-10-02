47e6f505d51862b0ceb7c0d7dc6db6116bed6f92
Brooks Ryba <brooksryba@gmail.com>
2022-10-02 19:06:02 -0400

    Working player hud with two buttons

--- Assets/Classes/Systems/InputSystem.cs
+++ Assets/Classes/Systems/InputSystem.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class InputSystem : MonoBehaviour
 {
@@ -9,17 +12,53 @@ public class InputSystem : MonoBehaviour
 
     private bool useOverlay = true;
     public GameObject overlayObject;
+    public GameObject hudObject;
+    public GameObject HUDLevelText;
+    public Slider HUDHealthSlider;
+    public Slider HUDManaSlider;
+    public GameObject HUDCurrencyText;
 
     void Start()
     {
         if(SystemInfo.deviceType != DeviceType.Handheld){
             useOverlay = false;
             overlayObject.SetActive(false);
-        }        
+        }    
+
+        InvokeRepeating("Refresh", 0.0f, 1f);    
+    }
+
+    void Refresh()
+    {
+        GameObject player = GameObject.Find("Player");
+        if(player == null)
+            return;
+
+        Character character = player.GetComponent<Character>();
+
+        HUDLevelText.GetComponent<TMP_Text>().SetText("Lvl. "+character.level);
+
+        HUDHealthSlider.minValue = 0;
+        HUDHealthSlider.maxValue = character.characterClass.maxHP;
+        HUDHealthSlider.value = character.currentHP;
+
+        HUDManaSlider.minValue = 0;
+        HUDManaSlider.maxValue = character.characterClass.maxMana;
+        HUDManaSlider.value = character.currentMana;   
+    }
+
+    public void ToggleMute() {
+        SettingsSystem.instance.ToggleMute();
+    }
+
+    public void MainMenu() {
+        SaveSystem.SaveAndDeregister();
+        SceneManager.LoadScene(sceneName: "Menu");
     }
 
     public void DisableControls()
     {
+        hudObject.SetActive(false);
         if(useOverlay && overlayObject != null) {
             overlayObject.SetActive(false);
         }
@@ -27,6 +66,7 @@ public class InputSystem : MonoBehaviour
 
     public void EnableControls()
     {
+        hudObject.SetActive(true);
         if(useOverlay && overlayObject != null) {
             overlayObject.SetActive(true);
         }
